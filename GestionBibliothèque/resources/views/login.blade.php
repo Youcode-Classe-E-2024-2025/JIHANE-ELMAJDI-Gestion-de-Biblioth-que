49eff4785cd51eec079718e50c627b2c2b03fca5
JIHANE ELMAJDI <emleyamanda9@gmail.com>
2025-02-18 14:23:40 +0100

    amélioration de l'interface de la page de connexion

--- GestionBibliothèque/resources/views/login.blade.php
+++ GestionBibliothèque/resources/views/login.blade.php
@@ -1,1 +1,6 @@
-<h1>HI</h1>
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="password" placeholder="Mot de passe">
+    <button type="submit">Se connecter</button>
+</form>
